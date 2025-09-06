9ae3c669306237faae91a7472d0acc3b48cd8f14
sowert <kosmax1401@gmail.com>
2025-09-06 22:08:44 +0400

    faf

--- api/debug.js
+++ api/debug.js
@@ -1,8 +1,7 @@
-export const config = {
-  runtime: 'nodejs'
-};
+export const config = { runtime: 'nodejs' };
 
 export default function handler(request, response) {
+  console.log('DEBUG ENV:', process.env); // ← будет видно весь env
   response.json({
     hasApiKey: !!process.env.FACEIT_API_KEY,
     apiKeyLength: process.env.FACEIT_API_KEY?.length || 0,
